68fc9810c1ac0f62c9762c5ea793ae216881eecb
prabhakar <prabhakar7382@gmail.com>
2024-02-21 09:29:10 +0530

    installed parcel and setup it

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import React from"react";
+import ReactDOM from"react-dom/client";
+
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "This is namaste react"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
@@ -9,11 +13,7 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello World from React!"
-// );
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React
+
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement()
+- File Watching Algorithm - written in C++
+- Caching -  Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Slpitting
+- Differential Bundling - to support of older broweser
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - unused code for you
