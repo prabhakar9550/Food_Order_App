c701dd89bb90ce360590de6c815eef3e3a7b656b
prabhakar <prabhakar7382@gmail.com>
2024-02-25 10:13:19 +0530

    added State variable

---
+++ src/components/Body.js
@@ -0,0 +1,30 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState } from "react";
+
+const Body = () => {
+
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+
+
+    return (
+      <div className="body">
+          <div className="filter">
+            <button className="filter-btn" onClick={() => {
+          const   filteredList = listOfRestaurants.filter(
+            (res) => res.info.avgRating > 4.5);
+         
+            setListOfRestaurants(filteredList);
+            }}>Top Rated Rastaurent</button>
+          </div>
+          <div className="res-container">
+           {listOfRestaurants.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)}
+            
+           
+          </div>
+      </div>
+    )
+  };
+
+export default Body;
