c04ff91ea3bbcfebc7cecd58046c4b19a58bdd74
prabhakar <prabhakar7382@gmail.com>
2024-03-04 14:53:22 +0530

    class component

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,35 @@
 
-const About = () => {
-    return (
-        <div>
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+     constructor(props){
+        super(props)
+
+   //     console.log("Parent constructor")
+     }
+
+     componentDidMount(){
+     //   console.log("Parent Compontent Did Mount")
+     }
+
+    render(){
+     //   console.log("Parent Render")
+        return (
+            <div>
             <h1>About</h1>
             <h2>This is Namaste React Web Series</h2>
+           
+            <UserClass 
+             name={"Prabhakar (cls)"}
+             location={"Hyderabad (cls)"}
+            />
+         
         </div>
-    )
+        )
+    }
 }
+
+
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class UserClass extends React.Component {
+    constructor(props){
+       super(props);
+
+       this.state = {
+       userInfo: {
+        name: "Dummy",
+        location: "Default",
+        avatar_url: "",
+       },
+       
+       };
+     //  console.log("Child Constructor" );
+    }
+
+  async  componentDidMount(){
+      //  console.log("Child Compontent Did Mount")
+      const data = await fetch("https://api.github.com/users/prabhakar9550");
+      const json = await data.json();
+
+      this.setState({
+        userInfo: json,
+      });
+
+   //   console.log(json);
+     }
+
+     componentDidUpdate(){
+        console.log("component did update")
+     };
+
+
+    render(){
+        const {name, location, avatar_url} = this.state.userInfo;
+      
+
+    //    console.log(" Child Render");
+        return  (
+            <div className="user-card">
+            <img src={avatar_url} alt="user_logo"/>
+           <h2>Name: {name}</h2>
+           <h3>Location: {location}</h3>
+           <h4>Contact: @prabhakar</h4>
+       </div>
+       )
+    }
+}
+
+export default UserClass;
